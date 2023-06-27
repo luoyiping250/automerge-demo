f58336e96c48631b3a55d8bbd1987bef6d2fc0b8
luoyp98 <luoyp98@chinaunicom.cn>
2023-06-27 10:41:28 +0800

    feat(消息通信)逻辑优化

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,32 +1,42 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 import * as Automerge from '@automerge/automerge'
 import { Input } from 'antd';
 import socket from './socket';
 import './demo.css';
 
+
 function Demo() {
-  const [msg, setMsg] = useState('消息为空');
-  let doc = Automerge.init();
+  const [msg, setMsg] = useState('--');
+  // 初始化文本对象
+  let doc = useRef(Automerge.init());
 
   useEffect(() => {
 
-    // 监听消息
-    socket.addEventListener('message', (event) => {
+    // 监听文档更新消息
+    socket.onmessage = (event) => {
       const msgStr = event.data;
-      let changes = msgStr && JSON.parse(msgStr)?.changes;
-     if(changes){
-       // eslint-disable-next-line no-debugger
-       changes[0].length = Object.keys(changes[0])?.length;
-       const changesUnit8 = new Uint8Array(Array.from(changes[0]));
-       changes[0] = changesUnit8;
-       let [newDoc, patch] = Automerge.applyChanges(doc, changes);
-       // 解析文档内容
-       const text1 = newDoc.ideas[0]?.toJSON();
-       doc = newDoc;
-       setMsg(text1);
+      console.log('接收消息：' + msgStr);
+      
+     if(msgStr){
+       let msg = msgStr && JSON.parse(msgStr);
+       const {type, changes} = msg;
+       if(type == 'update' && changes){
+        // 特殊处理：将普通数组changes转为Uint8Array
+        changes[0].length = Object.keys(changes[0])?.length;
+        const changesUnit8 = new Uint8Array(Array.from(changes[0]));
+        changes[0] = changesUnit8;
+
+        // 解析文档内容更新
+        let [newDoc] = Automerge.applyChanges(doc.current, changes);
+        const newMsg = newDoc.text[0]?.toJSON();
+        setMsg(newMsg);
+
+        // 更新文档对象
+        doc.current = newDoc;
+       }
      }
-    });
+    };
 
   }, [])
 
@@ -40,13 +50,13 @@ function Demo() {
         <span className='title'>发消息：</span>
         <Input placeholder="Basic usage" onPressEnter={e => {
 
-          let newDoc = Automerge.change(Automerge.clone(doc), d => {
+          let newDoc = Automerge.change(Automerge.clone(doc.current), d => {
             // 设置文本初始内容
-              d.ideas = [new Automerge.Text(e.target.value)];
+              d.text = [new Automerge.Text(e.target.value)];
           })
 
           // 获取文档内容变化
-          let changes = Automerge.getChanges(doc, newDoc);
+          let changes = Automerge.getChanges(doc.current, newDoc);
 
           // 通知changes
           if(changes?.length > 0){
@@ -57,8 +67,8 @@ function Demo() {
             socket.send(msg ? JSON.stringify(msg) : msg);
           }
 
-          doc = newDoc;
-
+          // 更新文档对象
+          doc.current = newDoc;
         }}/>
       </div>
     </div>
--- src/socket.js
+++ src/socket.js
@@ -1,21 +1,15 @@
 let socket = new WebSocket("ws://192.168.153.143:8080/websocket/" + Math.random().toString());
 
-socket.onopen = function()
-{
-  // Web Socket 已连接上，使用 send() 方法发送数据
-  socket.send("发送数据");
-  console.log("数据发送中...");
+socket.onopen = function(){
+  console.log("websocket已连接");
 };
 
-socket.onmessage = function (evt) 
-{ 
-  console.log("数据已接收...");
+socket.onclose = function(){ 
+  console.log("websocket已关闭"); 
 };
 
-socket.onclose = function()
-{ 
-  // 关闭 websocket
-  console.log("连接已关闭..."); 
+socket.onerror = function(error){ 
+  console.log("websocket错误：" + error); 
 };
 
 export default socket;
